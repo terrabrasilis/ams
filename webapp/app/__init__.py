5f1bd136252262e89d0dc74bcf9080284918cb12
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2021-04-08 16:48:35 -0300

    adding flask cors.

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from ams.dataaccess import AlchemyDataAccess
 from .config import Config
 
@@ -7,6 +8,7 @@ bp = Blueprint('', __name__)
 
 def create_app(config=Config):
 	app = Flask(__name__)
+	cors = CORS(app)	
 	app.config.from_object(config)	
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	db.connect(config.DATABASE_URL)
