57e1f9370941f290dd2e46334f4505cf8b1e26cc
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2021-04-27 11:58:30 -0300

    Close #25 - Add cells 300km spatial unit controllers.

--- geoserver/sqlviews/csAmz-150km-view.sql
+++ geoserver/sqlviews/csAmz-150km-view.sql
@@ -1,6 +1,7 @@
--- classname:  \b[A-Z]{2}\b
--- date: \d{4}-\d{2}-\d{2}
--- limit: (ALL|\d+)
+-- name: default - regex
+-- classname: DS - \b[A-Z]{2}\b
+-- date: 2021-01-01 - \d{4}-\d{2}-\d{2}
+-- limit: ALL - (ALL|\d+)
 
 SELECT 
 	su.suid, su.id, su.geometry, ri.classname, ri.date, COALESCE(ri.total, 0) AS percentage
