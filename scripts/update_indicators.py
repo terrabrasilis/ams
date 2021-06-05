5546d4fa1d5ba0dda1a97ccb3f63a186db6aa9b3
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2021-06-05 12:13:44 -0300

    adding a script for updating deter data.

---
+++ scripts/update_indicators.py
@@ -0,0 +1,16 @@
+from ams.usecases import DeterDailyUpdate, Initializer
+from ams.repository import (DeterRepository,
+							SpatialUnitInfoRepository)
+from ams.dataaccess import AlchemyDataAccess
+
+
+db_url = 'postgresql://postgres:postgres@localhost:5432/AMS'
+da = AlchemyDataAccess()
+da.connect(db_url)
+su_info_repo = SpatialUnitInfoRepository(da)
+init = Initializer(su_info_repo)
+init.execute(da)
+deter_repo = DeterRepository()
+update = DeterDailyUpdate(deter_repo, 60)
+update.execute(da)
+update.terminate()
