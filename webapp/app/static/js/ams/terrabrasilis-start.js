0508cb61f0079cf8719df844cb066543e2c86cb8
André Carvalho <afacarvalho@yahoo.com.br>
2021-06-18 19:07:49 -0300

    Template adjusts

--- webapp/app/static/js/ams/terrabrasilis-start.js
+++ webapp/app/static/js/ams/terrabrasilis-start.js
@@ -1,28 +1,39 @@
 $(document).ready(function () {
+
+  let toggleSideBar=()=> {
+    $('#sidebar, #content').toggleClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    // no sense because when side bar has class active the return is false
+    if($('#sidebar').hasClass('active')){// if open
+        $('#panel_container').addClass('full-width');
+    }else{//if close
+        $('#panel_container').removeClass('full-width');
+    }
+  };
+
+  let setMapHeight=()=>{
+    $('#map').height(window.innerHeight-$('footer.footer').height()-$('#content').height());
+  };
     
   $("#sidebar").mCustomScrollbar({
       theme: "minimal"
   });
 
-  $('#sidebarCollapse, #prepare_print').on('click', function () {
-      $('#sidebar, #content').toggleClass('active');
-      $('.collapse.in').toggleClass('in');
-      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-      // no sense because when side bar has class active the return is false
-      if($('#sidebar').hasClass('active')){// if open
-          $('#panel_container').addClass('full-width');
-      }else{//if close
-          $('#panel_container').removeClass('full-width');
-      }
-      // TODO: if the application context needs to be resized, call the resize function here
-  });
+  $('#sidebarCollapse, #prepare_print').on('click', toggleSideBar);
+
+  // close side bar by default
+  toggleSideBar();
+  // set map div to available height
+  setMapHeight();
+  // start webapp based on definitions inside index.html
+  startApp();
 
   /** display app version on footer bar */
   let versionDiv = $('#version');
   if(versionDiv.length>0){
-    versionDiv.append('ver: 1.0.0');
     // TODO: enable this code if a file with tag version is present
-    $.getJSON('PROJECT_VERSION', function(data) {
+    $.getJSON('static/PROJECT_VERSION', function(data) {
         let version = data.version;
         versionDiv.append('ver: '+version);
     });
