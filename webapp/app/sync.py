890098311c3c466437604af577cc9bbfa388a38f
André Carvalho <afacarvalho@yahoo.com.br>
2021-11-30 14:21:31 -0300

    Remove unused files

--- webapp/app/sync.py
+++
@@ -1,9 +0,0 @@
-from ams.dataaccess import AlchemyDataAccess
-from .controllers import InitializerController
-from .config import Config
-
-db = AlchemyDataAccess()
-
-def startSync(config=Config):
-  db.connect(config.DATABASE_URL)
-  InitializerController(db, config.DETER_DAILY_UPDATE_TIME_INTERVAL)
