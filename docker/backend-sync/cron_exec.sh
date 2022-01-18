50f03125668e2cb296108f0c7e15e6a272a7eef1
André Carvalho <afacarvalho@yahoo.com.br>
2022-01-18 17:34:34 -0300

    Improve log for cron exec

--- docker/backend-sync/cron_exec.sh
+++ docker/backend-sync/cron_exec.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 cd /usr/local/scripts/
 source .env
+# write new date into log before run update
+echo "====================================="
+DT=$(date)
+echo ${DT}
+echo "====================================="
 /usr/local/bin/python3 update_indicators.py
