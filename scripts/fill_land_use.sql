5cbb5ecdbf1b453da614f0ea8c5431a87aaa3570
André Carvalho <afacarvalho@yahoo.com.br>
2022-01-24 14:36:06 -0300

    Adding the priority column to define an order in which items are presented.

--- scripts/fill_land_use.sql
+++ scripts/fill_land_use.sql
@@ -2,12 +2,13 @@ drop table if exists land_use;
 CREATE TABLE public.land_use (
 	id serial2 NOT NULL,
 	"name" varchar NULL,
+	priority integer NULL,
 	CONSTRAINT land_use_pk PRIMARY KEY (id)
 );
-INSERT INTO land_use values(1,'APA');
-INSERT INTO land_use values(2,'Assentamentos');
-INSERT INTO land_use values(3,'CAR');
-INSERT INTO land_use values(4,'FPND');
-INSERT INTO land_use values(5,'TI');
-INSERT INTO land_use values(6,'UC');
-INSERT INTO land_use values(12,'Indefinida');
+INSERT INTO land_use values(1,'APA',0);
+INSERT INTO land_use values(2,'Assentamentos',1);
+INSERT INTO land_use values(3,'CAR',2);
+INSERT INTO land_use values(4,'FPND',3);
+INSERT INTO land_use values(5,'TI',4);
+INSERT INTO land_use values(6,'UC',5);
+INSERT INTO land_use values(12,'Indefinida',6);
