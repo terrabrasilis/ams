3f744aaa1e8a1c80d52eff6a4397423e60ae2cae
andre-carvalho <afacarvalho@yahoo.com.br>
2022-02-24 20:29:56 -0300

    Change get environment vars to support secrets from swram

--- webapp/app/config.py
+++ webapp/app/config.py
@@ -11,3 +11,6 @@ class Config:
 	SERVER_NAME = os.environ.get('SERVER_NAME') or '127.0.0.1:5000'
 	APPLICATION_ROOT = os.environ.get('APPLICATION_ROOT') or ''
 	INPUT_GEOTIFF_FUNDIARY_STRUCTURE = os.environ.get('INPUT_GEOTIFF_FUNDIARY_STRUCTURE') or 'est_fundiaria_cst.tif'
+
+	if os.path.exists(DATABASE_URL):
+		DATABASE_URL = open(DATABASE_URL, 'r').read()
