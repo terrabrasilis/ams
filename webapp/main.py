264f10c8d22a86883311c7f8ce146235f1b70184
andre-carvalho <afacarvalho@yahoo.com.br>
2022-05-11 11:48:27 -0300

    Clear unused comment.

--- webapp/main.py
+++ webapp/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 path = os.path.join(os.path.dirname(__file__), os.pardir)
 sys.path.append(path)
-from app import create_app  # noqa: E402
+from app import create_app
 
 
 app = create_app()
