6a0033e60d104d90752a3680622c62cb79c9aa9d
andre-carvalho <afacarvalho@yahoo.com.br>
2022-05-13 10:08:36 -0300

    Set statistics processing only to new data

--- scripts/update_indicators.py
+++ scripts/update_indicators.py
@@ -9,7 +9,7 @@ from ams.usecases import DeterDaily
 from ams.usecases.classify_by_land_use import ClassifyByLandUse
 
 # update all data including deter history. For Active Fires, use all data from raw database.
-alldata=True
+alldata=False
 
 deterupdate = DeterDaily(Config.DATABASE_URL, alldata)
 deterupdate.execute()
