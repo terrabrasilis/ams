a508247fc117669a41743ff1fad40f080863cecd
andre-carvalho <afacarvalho@yahoo.com.br>
2022-09-21 19:53:55 -0300

    Change configuration to use in amazonia backend

--- webapp/app/config.py
+++ webapp/app/config.py
@@ -5,14 +5,14 @@ class Config:
 	HOMOLOGATION = os.environ.get('HOMOLOGATION') or False
 	GEOSERVER_URL = os.environ.get('GEOSERVER_URL') or \
 			'http://localhost:8080/geoserver'
-	GEOSERVER_WORKSPACE = os.environ.get('GEOSERVER_WORKSPACE') or 'ces'
+	GEOSERVER_WORKSPACE = os.environ.get('GEOSERVER_WORKSPACE') or 'ams'
 	DATABASE_URL = os.environ.get('DATABASE_URL') or \
-			'postgresql://postgres:postgres@localhost:5432/CES'
+			'postgresql://postgres:postgres@localhost:5432/AMS'
 	SERVER_NAME = os.environ.get('SERVER_NAME') or '127.0.0.1:5000'
 	APPLICATION_ROOT = os.environ.get('APPLICATION_ROOT') or ''
 	INPUT_GEOTIFF_FUNDIARY_STRUCTURE = os.environ.get('INPUT_GEOTIFF_FUNDIARY_STRUCTURE') or \
-		'estrutura_fundiaria_cerrado_cst_lzw.tif'
-	BIOME = os.environ.get('BIOME') or "'Cerrado'"
+		'estrutura_fundiaria_cst_2022.tif'
+	BIOME = os.environ.get('BIOME') or "'Amazônia'"
 	ALL_DATA = os.environ.get('ALL_DATA') or True
 
 	if os.path.exists(DATABASE_URL):
