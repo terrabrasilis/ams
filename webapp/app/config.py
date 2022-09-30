646006bf72a93d1423de8d231d64dd6f4e57f174
andre-carvalho <afacarvalho@yahoo.com.br>
2022-09-30 10:19:39 -0300

    Reorder config keys

--- webapp/app/config.py
+++ webapp/app/config.py
@@ -5,17 +5,16 @@ class Config:
 	GEOSERVER_URL = os.environ.get('GEOSERVER_URL') or 'http://localhost:8080/geoserver'
 	SERVER_NAME = os.environ.get('SERVER_NAME') or '127.0.0.1:5000'
 	APPLICATION_ROOT = os.environ.get('APPLICATION_ROOT') or ''
+	# used when frontend biome changes
+	DB_CERRADO_URL = os.environ.get('DB_CERRADO_URL') or 'postgresql://postgres:postgres@localhost:5432/CES'
+	DB_AMAZON_URL = os.environ.get('DB_AMAZON_URL') or 'postgresql://postgres:postgres@localhost:5432/AMS'
 
 	# used for backend process
 	INPUT_GEOTIFF_FUNDIARY_STRUCTURE = os.environ.get('INPUT_GEOTIFF_FUNDIARY_STRUCTURE') or 'estrutura_fundiaria_cst_2022.tif'
 	BIOME = os.environ.get('BIOME') or "'Amazônia'"
 	ALL_DATA = os.environ.get('ALL_DATA')=='True'
-
-	# both use
 	DATABASE_URL = os.environ.get('DATABASE_URL') or 'postgresql://postgres:postgres@localhost:5432/AMS'
 	if os.path.exists(DATABASE_URL):
 		DATABASE_URL = open(DATABASE_URL, 'r').read()
 	
-	# used when frontend biome changes
-	DB_CERRADO_URL = os.environ.get('DB_CERRADO_URL') or 'postgresql://postgres:postgres@localhost:5432/CES'
-	DB_AMAZON_URL = os.environ.get('DB_AMAZON_URL') or 'postgresql://postgres:postgres@localhost:5432/AMS'
+	
