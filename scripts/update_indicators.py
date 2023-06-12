909656103322aae5f44f933e93a8333de499d834
andre-carvalho <afacarvalho@yahoo.com.br>
2023-06-12 18:06:13 -0300

    Changes to the daily ETL entry point to include updating risk data.

--- scripts/update_indicators.py
+++ scripts/update_indicators.py
@@ -6,6 +6,8 @@ warnings.filterwarnings("ignore")
 from webapp.app.config import Config
 from ams.usecases import ActiveFires
 from ams.usecases import DeterDaily
+from ams.dataaccess.ftp_ibama_risk import FtpIBAMARisk
+from ams.usecases import IBAMARisk
 from ams.usecases.classify_by_land_use import ClassifyByLandUse
 
 # Update all data including deter history? (see at: ~/ams/webapp/app/config.py)
@@ -17,6 +19,12 @@ deterupdate.execute()
 
 firesupdate = ActiveFires(Config.DATABASE_URL, Config.BIOME, alldata)
 firesupdate.execute()
+
+# only for Amazonia
+if Config.BIOME=='Amazônia':
+    ftp = FtpIBAMARisk(Config.DATABASE_URL, host=Config.FTP_HOST, ftp_path=Config.FTP_PATH, user=Config.FTP_USER, password=Config.FTP_PASS, port=Config.FTP_PORT, output_path=Config.RISK_OUTPUT_PATH, risk_file_name=Config.RISK_INPUT_FILE)
+    riskupdate = IBAMARisk(Config.DATABASE_URL, ftp=ftp)
+    riskupdate.execute()
 
 class_deter_polys = ClassifyByLandUse(Config.DATABASE_URL, Config.INPUT_GEOTIFF_FUNDIARY_STRUCTURE, alldata)
 class_deter_polys.execute()
