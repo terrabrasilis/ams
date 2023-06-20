8d436b5ba605a85ef56c6df9443a3d2358b8c132
andre-carvalho <afacarvalho@yahoo.com.br>
2023-06-20 11:13:00 -0300

    Adding support to read the secrets from swarm cluster

--- webapp/app/config.py
+++ webapp/app/config.py
@@ -24,10 +24,20 @@ class Config:
 	
 	# used for ETL of IBAMA risk
 	FTP_HOST = os.environ.get('FTP_HOST') or "ftp.com.br"
+	if os.path.exists(FTP_HOST):
+		FTP_HOST = open(FTP_HOST, 'r').read()
 	FTP_PORT = os.environ.get('FTP_PORT') or "21"
+	if os.path.exists(FTP_PORT):
+		FTP_PORT = open(FTP_PORT, 'r').read()
 	FTP_PATH = os.environ.get('FTP_PATH') or "/"
+	if os.path.exists(FTP_PATH):
+		FTP_PATH = open(FTP_PATH, 'r').read()
 	FTP_USER = os.environ.get('FTP_USER') or "user"
+	if os.path.exists(FTP_USER):
+		FTP_USER = open(FTP_USER, 'r').read()
 	FTP_PASS = os.environ.get('FTP_PASS') or "secret_password"
+	if os.path.exists(FTP_PASS):
+		FTP_PASS = open(FTP_PASS, 'r').read()
 	# local where write the file copied from ftp
 	RISK_OUTPUT_PATH = os.environ.get('RISK_OUTPUT_PATH') or "/"
 	RISK_INPUT_FILE = os.environ.get('RISK_INPUT_FILE') or "Risk_areas_AMZL.tif"
